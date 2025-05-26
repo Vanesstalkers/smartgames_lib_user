848db39366cec2499c26b0e3aff242a240abfa95
VanesStalkers <ivan.mironov@bk.ru>
2025-05-27 02:19:03 +0400

    fix lobbyGameConfigs update

--- api/update.js
+++ api/update.js
@@ -27,7 +27,9 @@ async (
   if (lobbyGameConfigs !== undefined) setData.lobbyGameConfigs = lobbyGameConfigs;
 
   if (Object.keys(setData).length) {
-    user.set(setData);
+    user.set(setData, {
+      removeEmptyObject: true // иначе при старте каждой игры будет приходить одинаковый конфиг и после lib.utils.mergeDeep изменения запишутся в БД пустым объектом
+    });
     await user.saveChanges();
     await user.updateUserCache(cacheData);
   }
